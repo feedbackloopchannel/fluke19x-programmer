4ed35181b29bf42d27863b30abdee2dd7b002c6e
feedbackloopchannel <feedback.loop.channel@gmail.com>
2023-05-06 17:46:49 -0700

    quick test read and write, model and SN, separate error counts for U1 and U2

--- programmer.py
+++ programmer.py
@@ -12,6 +12,12 @@ parser.add_argument('-r', '--read', default = '')
 parser.add_argument('-w', '--write', default = '')
 # old version has 16Mbit (1M words) Flash chips (as opposed to 8Mbit in new version)
 parser.add_argument('-o', '--old', nargs = '?', const = True, default = False)
+
+parser.add_argument('-R', '--Read', nargs = '?', const = True, default = False) # for testing
+parser.add_argument('-W', '--Write', nargs = '?', const = True, default = False) # for testing
+
+parser.add_argument('-s', '--scopemeter', nargs = '?', const = True, default = False) # read model and serial number
+
 args = parser.parse_args()
 
 flash_size_words = 512 * 1024
@@ -22,8 +28,10 @@ ser = serial.Serial(args.device, 115200, timeout = 20)
 
 if args.test: # RAM test
   ser.write(b't')
-  data = ser.read(4)
-  print("error count: {}".format(int.from_bytes(data, 'little')))
+  errors_u1 = ser.read(4)
+  errors_u2 = ser.read(4)
+  print("error count U1: {}".format(int.from_bytes(errors_u1, 'little')))
+  print("error count U2: {}".format(int.from_bytes(errors_u2, 'little')))
 
 elif args.id:
   ser.write(b'i')
@@ -74,11 +82,73 @@ elif len(args.write) > 0:
       count = 0
       for i in range(size_words * 4):
         data = f.read(1)
-        ser.write(data);
+        ser.write(data)
         count += 1
         if count == 16 * 1024:
           print('.', end = '', flush = True)
           count = 0
       print()
+
+elif args.Read:
+  ser.write(b'r')
+  start_addr = 0
+  size_words = 64
+  ser.write(start_addr.to_bytes(4, 'little'))
+  ser.write(size_words.to_bytes(4, 'little'))
+  addr = 0
+  count = 0
+  for i in range(size_words * 4):
+    if count == 0:
+      print("{0:08x}".format(addr), end = '')
+    data = ser.read(1)
+    print(" {0:02x}".format(int.from_bytes(data, 'little')), end = '')
+    count += 1
+    if count == 16:
+      print()
+      count = 0
+      addr += 16
+  print()
+
+elif args.Write:
+  ser.write(b'w')
+  start_addr = 1
+  size_words = 8
+  ser.write(start_addr.to_bytes(4, 'little'))
+  ser.write(size_words.to_bytes(4, 'little'))
+  ser.write(0x11223344.to_bytes(4, 'big'));
+  ser.write(0x55667788.to_bytes(4, 'big'));
+  ser.write(0x88776655.to_bytes(4, 'big'));
+  ser.write(0x44332211.to_bytes(4, 'big'));
+  ser.write(0x11223344.to_bytes(4, 'big'));
+  ser.write(0x55667788.to_bytes(4, 'big'));
+  ser.write(0x88776655.to_bytes(4, 'big'));
+  ser.write(0x44332211.to_bytes(4, 'big'));
+
+elif args.scopemeter:
+  # model
+  ser.write(b'r')
+  start_addr = 0x201a
+  size_words = 1
+  ser.write(start_addr.to_bytes(4, 'little'))
+  ser.write(size_words.to_bytes(4, 'little'))
+  model = ''
+  for i in range(size_words * 4):
+    data = ser.read(1)
+    if data != 0:
+      model = model + data.decode('utf-8')
+  print("Model: {}".format(model))
+
+  # serial number
+  ser.write(b'r')
+  start_addr = 0x2022
+  size_words = 2
+  ser.write(start_addr.to_bytes(4, 'little'))
+  ser.write(size_words.to_bytes(4, 'little'))
+  sn = ''
+  for i in range(size_words * 4):
+    data = ser.read(1)
+    if data != 0:
+      sn = sn + data.decode('utf-8')
+  print("SN: {}".format(sn))
 
 ser.close()
